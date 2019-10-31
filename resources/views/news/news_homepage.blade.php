69baf7c4ee81c7763102badc533bf7ae011967a3
Fritz Mawusi <eabventures@gmail.com>
2019-10-31 16:29:32 +0000

    news tab-1

--- resources/views/news/news_homepage.blade.php
+++ resources/views/news/news_homepage.blade.php
@@ -6,16 +6,16 @@
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
      <style type="text/css">
         .navbar {
-          min-height: 10px;
+          min-height: 1px;
         }
         .navbar-brand {
-          padding-top: 10px;
-          padding-top: 10px;
+          padding-top: 17px;
+          padding-top: 17px;
           line-height: 15px;
         }
         .navbar-toggle {
           /* (80px - button height 34px) / 2 = 23px */
-          margin-top: 13px;
+          margin-top: 23px;
           padding: 9px 10px !important;
         }
         @media (min-width: 768px) {
@@ -23,13 +23,12 @@
             /* (80px - line-height of 27px) / 2 = 26.5px */
             padding-top: 10px;
             padding-bottom: 10px;
-            line-height: 10px;
+            line-height: 17px;
           }
         }
         .content {
             padding: 0.1px;
         }
-        .gi-2x{font-size: 0.1em;}
     </style>
 @endsection
 
