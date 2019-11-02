5ca2327d2f116d9af73ae9681a19e65e54e84ac8
Fritz Mawusi <eabventures@gmail.com>
2019-11-02 09:49:50 +0000

    resized second tab for 4th

--- resources/views/pre_1992_legislation/displayed_all_acts_view.blade.php
+++ resources/views/pre_1992_legislation/displayed_all_acts_view.blade.php
@@ -21,9 +21,9 @@
         @media (min-width: 768px) {
           .navbar-nav > li > a {
             /* (80px - line-height of 27px) / 2 = 26.5px */
-            padding-top: 17px;
-            padding-bottom: 17px;
-            line-height: 15px;
+            padding-top: 10px;
+            padding-bottom: 10px;
+            line-height: 10px;
           }
         }
         .content {
