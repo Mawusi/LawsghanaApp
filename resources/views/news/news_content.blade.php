8f593b62c93419415c88e339d082b5c43c9041c9
Fritz Mawusi <eabventures@gmail.com>
2019-11-10 02:05:27 +0000

    changes in logo

--- resources/views/news/news_content.blade.php
+++ resources/views/news/news_content.blade.php
@@ -16,7 +16,7 @@
             {{ $newsContent['title'] }}
             </h1>
             <p class="mb-5">
-              <img src="{{ asset('/storage_old/pages/page1.jpg') }}" alt="Image" class="img-fluid">
+              <img src="{{ asset('/logo/page1.jpg') }}" alt="Image" class="img-fluid">
             </p>  
             <div class="post-meta d-flex mb-5">
               <div class="bio-pic mr-3">
@@ -166,7 +166,7 @@
             </div>
               <!-- <div class="number align-self-start">{{ $newsContent->id }}</div> -->
               <div class="trend-contents">
-                <h2><a href="/{{ $newsContent->news_category }}/{{ $newsContent->title }}/{{ $newsContent->id }}">{{ $newsContent->title }}</a></h2>
+                <h2><a href="/News/{{ $newsContent->news_category }}/{{ $newsContent->title }}/{{ $newsContent->id }}">{{ $newsContent->title }}</a></h2>
                 <div class="post-meta">
                   <span class="d-block"><a href="#">Dave Rogers</a> in <a href="#">News</a></span>
                   <span class="date-read">Jun 14 <span class="mx-1">&bullet;</span> 3 min read <span class="icon-star2"></span></span>
