8cca9534d6ac9607d5195779f66515b2944e8c6b
Fritz Mawusi <eabventures@gmail.com>
2019-11-14 00:02:17 +0000

    remove small caption

--- resources/views/news/news_content.blade.php
+++ resources/views/news/news_content.blade.php
@@ -22,10 +22,10 @@
               <div class="bio-pic mr-3">
                 <img src="{{ asset('/logo/images.jpg') }}" alt="Image" class="img-fluid">
               </div>
-              <div class="vcard">
+              <!-- <div class="vcard">
                 <span class="d-block"><a href="#">Dave Rogers</a> in <a href="#">News</a></span>
                 <span class="date-read">Jun 14 <span class="mx-1">&bullet;</span> 3 min read <span class="icon-star2"></span></span>
-              </div>
+              </div> -->
             </div>
                 {!! $newsContent['content'] !!}
 
