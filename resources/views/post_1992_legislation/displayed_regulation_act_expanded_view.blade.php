e7099bfb02a3a8892efebea2e60cc70616960591
Fritz Mawusi <eabventures@gmail.com>
2020-02-05 13:12:05 +0000

    remove print options for post

--- resources/views/post_1992_legislation/displayed_regulation_act_expanded_view.blade.php
+++ resources/views/post_1992_legislation/displayed_regulation_act_expanded_view.blade.php
@@ -1,7 +1,7 @@
 
 <div>
   <div style="margin-bottom: 5px;">
-      <a class="pull-right" id="print_options" href="#">Print Options</a>
+      {{-- <a class="pull-right" id="print_options" href="#">Print Options</a> --}}
       <div class="menu_options pull-right" style="display: none;">
           <a href="/post_1992_legislation/pdf/regulation/expanded/{{$regulationAct['act_category']}}/{{$regulationAct['title']}}/{{ $regulationAct['id'] }}"><img alt="Brand" src="{{ asset('/logo/pdf.png') }}" style="width:1.5em;">&nbsp;PDF</a>&nbsp;&nbsp;||&nbsp;
           <a href="/post_1992_legislation/plain/regulation/expanded/{{$regulationAct['act_category']}}/{{$regulationAct['title']}}/{{ $regulationAct['id'] }}" target="_blank">Plain View</a>&nbsp;&nbsp;||&nbsp;
