65b3caf5b5cb90b3745791d4e0e611f3f32afe1a
Fritz Mawusi <eabventures@gmail.com>
2020-03-06 17:53:37 +0000

    few on login updates

--- resources/views/post_1992_legislation/displayed_amended_act_content.blade.php
+++ resources/views/post_1992_legislation/displayed_amended_act_content.blade.php
@@ -4,11 +4,41 @@
     </div>
 
     <div style="margin-bottom: 5px;">
-        <a id="print_options" href="#">Print Options</a>
+        &nbsp;&nbsp;&nbsp;&nbsp;<a id="print_options" href="#">Print Options</a>
         <div class="menu_options pull-right" style="display: none;">
-            <a href="/post_1992_legislation/pdf/amended_content_section/{{$amendedContent['act_title']}}/{{ $amendedContent['id'] }}"><img alt="Brand" src="{{ asset('/logo/pdf.png') }}" style="width:1.5em;">&nbsp;PDF</a>&nbsp;&nbsp;||&nbsp;
-            {{-- <a href="/post_1992_legislation/plain_amended/content_section/{{ $amendedContent['id'] }}" target="_blank">Plain View</a>&nbsp;&nbsp;||&nbsp; --}}
-            <a href="/post_1992_legislation/print_amended/content_section/{{ $amendedContent['id'] }}" target="_blank"><span class="glyphicon glyphicon-print" aria-hidden="true"></span>&nbsp;Print Preview</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            @if (Route::has('login'))
+                @auth
+                    <a href="/post_1992_legislation/pdf/amended_content_section/{{$amendedContent['act_title']}}/{{ $amendedContent['id'] }}"><img alt="Brand" src="{{ asset('/logo/pdf.png') }}" style="width:1.5em;">&nbsp;PDF</a>&nbsp;&nbsp;||&nbsp;
+                    {{-- <a href="/post_1992_legislation/plain_amended/content_section/{{ $amendedContent['id'] }}" target="_blank">Plain View</a>&nbsp;&nbsp;||&nbsp; --}}
+                    <a href="/post_1992_legislation/print_amended/content_section/{{ $amendedContent['id'] }}" target="_blank"><span class="glyphicon glyphicon-print" aria-hidden="true"></span>&nbsp;Print</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        
+                    @else
+
+                    <a href="" data-toggle="modal" data-target="#myModal"><img alt="Brand" src="{{ asset('/logo/pdf.png') }}" style="width:1.5em;">&nbsp;PDF</a>&nbsp;&nbsp;||&nbsp;
+                    {{-- <a href="/post_1992_legislation/plain_amended/content_section/{{ $amendedContent['id'] }}" target="_blank">Plain View</a>&nbsp;&nbsp;||&nbsp; --}}
+                    <a href="" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-print" aria-hidden="true"></span>&nbsp;Print</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        
+                    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                        <div class="modal-dialog" role="document">
+                          <div class="modal-content">
+                            <div class="modal-header">
+                              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                              <h4 class="modal-title" id="myModalLabel">Kindly <span style="color:#3490dc;">Log In</span> or <span style="color:#3490dc;">Register</span> to Create An Account</h4>
+                            </div>
+                            <div class="modal-body">
+                                <a class="btn btn-sm btn-primarys" href="{{ route('login') }}">Login</a>
+                                <a class="btn btn-sm btn-primarys" href="{{ route('register') }}">Register</a>                            
+                            </div>
+                            <div class="modal-footer">
+                              <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                              {{-- <button type="button" class="btn btn-primary">Save changes</button> --}}
+                            </div>
+                          </div>
+                        </div>
+                    </div>
+
+                @endauth
+            @endif
         </div>
     </div>
 
--- resources/views/post_1992_legislation/displayed_regulation_act_content.blade.php
+++ resources/views/post_1992_legislation/displayed_regulation_act_content.blade.php
@@ -1,6 +1,5 @@
 
 <div>
-
     <div class="header_only" style="border: .1px solid #ddd;">
         <p style="padding-top: 7px; padding-bottom: .1px; padding-left: 16px; padding-right: 16px;"><b>{{ $regulationAct['section'] }}</b></p>
     </div>
@@ -16,21 +15,24 @@
 
                     @else
 
-                    <a href="" data-toggle="modal" data-target=".bs-example-modal-lg"><img alt="Brand" src="{{ asset('/logo/pdf.png') }}" style="width:1.5em;">&nbsp;PDF</a>&nbsp;&nbsp;||&nbsp;
-                    <a href="" data-toggle="modal" data-target=".bs-example-modal-lg"><span class="glyphicon glyphicon-print" aria-hidden="true"></span>&nbsp;Print</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <a href="" data-toggle="modal" data-target="#myModals"><img alt="Brand" src="{{ asset('/logo/pdf.png') }}" style="width:1.5em;">&nbsp;PDF</a>&nbsp;&nbsp;||&nbsp;
+                    <a href="" data-toggle="modal" data-target="#myModals"><span class="glyphicon glyphicon-print" aria-hidden="true"></span>&nbsp;Print</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
-                    <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
-                        <div class="modal-dialog modal-lg" role="document">
-                            <div class="modal-content">
-                                <br>
-                                <div class="container">
-                                    <h3>Kindly <span style="color:blue;"><b>Log In</b></span> or <span style="color:blue;"><b>Register</b></span> to Create An Account</h3>
-                                    <br>
-                                    <a class="btn btn-sm btn-primary" href="{{ route('login') }}">Login</a>
-                                    <a class="btn btn-sm btn-primary" href="{{ route('register') }}">Register</a>
-                                </div>
-                                <br>
+                    <div class="modal fade" id="myModals" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                        <div class="modal-dialog" role="document">
+                          <div class="modal-content">
+                            <div class="modal-header">
+                              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                              <h4 class="modal-title" id="myModalLabel">Kindly <span style="color:#3490dc;">Log In</span> or <span style="color:#3490dc;">Register</span> to Create An Account</h4>
                             </div>
+                            <div class="modal-body">
+                                <a class="btn btn-sm btn-primarys" href="{{ route('login') }}">Login</a>
+                                <a class="btn btn-sm btn-primarys" href="{{ route('register') }}">Register</a>                            
+                            </div>
+                            <div class="modal-footer">
+                              <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                            </div>
+                          </div>
                         </div>
                     </div>
                     
