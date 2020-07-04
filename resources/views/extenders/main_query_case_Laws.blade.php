d629876ba09ba7b8ea74dfc6ccbb55d086dc00fa
Fritz Mawusi <eabventures@gmail.com>
2020-07-04 21:45:19 +0000

    on main search for cases

--- resources/views/extenders/main_query_case_Laws.blade.php
+++ resources/views/extenders/main_query_case_Laws.blade.php
@@ -2,7 +2,8 @@
     @foreach ($supreme_court_cases as $supreme_court_case)
     <div class="search-well">
     <h5 class="all_specific"><b>Case Laws</b></h5>
-    <a href="/judgement/plain_view/{{$supreme_court_case->id}}" target="_blank"><h5 style="color:blue;"><b>{!! $supreme_court_case->case_title !!}</b></h5></a>
+    {{-- <a href="/judgement/plain_view/{{$supreme_court_case->id}}" target="_blank"><h5 style="color:blue;"><b>{!! $supreme_court_case->case_title !!}</b></h5></a> --}}
+    <a><h5 style="color:blue;"><b>{!! $supreme_court_case->case_title !!}</b></h5></a>
         <b>{!! $supreme_court_case->gh_law_judgment_group_name !!} | {!! $supreme_court_case->reference_number !!}</b>
         <br><br>
         {!! str_limit(strip_tags(strstr($supreme_court_case->content,  $query, false)),450, '...' ) !!}
@@ -15,7 +16,8 @@
     @foreach ($court_of_appeal_cases as $court_of_appeal_case)
         <div class="search-well">
             <h5 class="all_specific"><b>Case Laws</b></h5>
-            <a href="/judgement/plain_view/{{$court_of_appeal_case->id}}" target="_blank"><h5 style="color:blue;"><b>{!! $court_of_appeal_case->case_title !!}</b></h5></a>
+            {{-- <a href="/judgement/plain_view/{{$court_of_appeal_case->id}}" target="_blank"><h5 style="color:blue;"><b>{!! $court_of_appeal_case->case_title !!}</b></h5></a> --}}
+            <a><h5 style="color:blue;"><b>{!! $court_of_appeal_case->case_title !!}</b></h5></a>
             <b>{!! $court_of_appeal_case->gh_law_judgment_group_name !!} | {!! $court_of_appeal_case->reference_number !!}</b>
             <br><br>
             {!! str_limit(strip_tags(strstr($court_of_appeal_case->content,  $query, false)),450, '...' ) !!}
@@ -28,7 +30,8 @@
     @foreach ($high_court_cases as $high_court_case)
         <div class="search-well">
             <h5 class="all_specific"><b>Case Laws</b></h5>
-            <a href="/judgement/plain_view/{{$high_court_case->id}}" target="_blank"><h5 style="color:blue;"><b>{!! $high_court_case->case_title !!}</b></h5></a>
+            {{-- <a href="/judgement/plain_view/{{$high_court_case->id}}" target="_blank"><h5 style="color:blue;"><b>{!! $high_court_case->case_title !!}</b></h5></a> --}}
+            <a><h5 style="color:blue;"><b>{!! $high_court_case->case_title !!}</b></h5></a>
             <b>{!! $high_court_case->gh_law_judgment_group_name !!} | {!! $high_court_case->reference_number !!}</b>
             <br><br>
             {!! str_limit(strip_tags(strstr($high_court_case->content,  $query, false)),450, '...' ) !!}
