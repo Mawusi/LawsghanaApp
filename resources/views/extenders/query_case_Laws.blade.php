8e2f90664dac2aa88350ee2ae0166b8e5fe47f56
Fritz Mawusi <eabventures@gmail.com>
2020-07-30 18:40:45 +0000

    on high court

--- resources/views/extenders/query_case_Laws.blade.php
+++ resources/views/extenders/query_case_Laws.blade.php
@@ -29,8 +29,8 @@
 <div class="only_high">
     @foreach ($high_court_cases as $high_court_case)
         <div class="search-well">
-        <a><h5 style="color:blue;"><b>{!! $high_court_case->case_title !!}</b></h5></a>
         <a href="/judgement/Ghana/{{$high_court_case->gh_law_judgment_group_name}}/{{$high_court_case->id}}" target="_blank"><h5 style="color:blue;"><b>{!! $high_court_case->case_title !!}</b></h5></a>
+        <b>{!! $high_court_case->gh_law_judgment_group_name !!} | {!! $high_court_case->reference_number !!}</b>
         <br><br>
             {!! str_limit(strip_tags(strstr($high_court_case->content,  $query, false)),450, '...' ) !!}
             {{-- {!! $court_of_appeal_case->content !!} --}}
