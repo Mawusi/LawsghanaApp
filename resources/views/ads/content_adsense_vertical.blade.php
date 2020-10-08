cc339c082ecae10d4049fc6002a0f72f6e683431
Fritz Mawusi <eabventures@gmail.com>
2020-10-08 18:10:45 +0000

    adsense cases again

--- resources/views/ads/content_adsense_vertical.blade.php
+++ resources/views/ads/content_adsense_vertical.blade.php
@@ -25,18 +25,3 @@
 <script>
     (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
-
-<br>
-
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- Lawsghana_sidebar_home_2 -->
-<ins class="adsbygoogle"
-    style="display:block"
-    data-ad-client="ca-pub-4293461101625028"
-    data-ad-slot="7874849965"
-    data-ad-format="auto"
-    data-full-width-responsive="true">
-</ins>
-<script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
