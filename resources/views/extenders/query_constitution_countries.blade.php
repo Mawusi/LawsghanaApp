f2ca6bbc721872ea40629833e041105ee719b36e
Fritz Mawusi <eabventures@gmail.com>
2020-11-03 15:12:37 +0000

    changes on africa search

--- resources/views/extenders/query_constitution_countries.blade.php
+++ resources/views/extenders/query_constitution_countries.blade.php
@@ -1,7 +1,7 @@
 <div class="only_africa">
     @foreach ($africa_countries_constitutions as $africa_countries_constitution)
     <div class="search-well">
-    <a href="/constitution/2/{{$africa_countries_constitution->continent}}/{{$africa_countries_constitution->country}}/{{$africa_countries_constitution->id}}" target="_blank"><h5 style="color:blue;"><b>{!! $africa_countries_constitution->title !!}</b></h5></a>
+    <a href="/constitution/1/{{$africa_countries_constitution->continent}}/{{$africa_countries_constitution->country}}/{{$africa_countries_constitution->id}}" target="_blank"><h5 style="color:blue;"><b>{!! $africa_countries_constitution->title !!}</b></h5></a>
         <b>{!! $africa_countries_constitution->country !!} - {!! $africa_countries_constitution->continent !!} | {!! $africa_countries_constitution->year !!}</b>
         <br>
         {!! str_limit(strip_tags(strstr($africa_countries_constitution->content,  $query, false)),450, '...' ) !!}
